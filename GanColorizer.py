7bda5f5fed6ad0aa3c50e69b2185fe19adef44cf
Tharun R  S <rstharun11@gmail.com>
2023-11-19 09:10:17 +0530

    update imports

--- GanColorizer.py
+++ GanColorizer.py
@@ -1,5 +1,4 @@
 import numpy as np
-import matplotlib.pyplot as plt
 from skimage.color import rgb2lab, lab2rgb
 from torch import nn, optim
 from torchvision import transforms
@@ -237,31 +236,6 @@ def lab_to_rgb(L, ab):
         rgb_imgs.append(img_rgb)
     return np.stack(rgb_imgs, axis=0)
 
-def visualize(model, data, save=True):
-    model.net_G.eval()
-    with torch.no_grad():
-        model.setup_input(data)
-        model.forward()
-    model.net_G.train()
-    fake_color = model.fake_color.detach()
-    real_color = model.ab
-    L = model.L
-    fake_imgs = lab_to_rgb(L, fake_color)
-    real_imgs = lab_to_rgb(L, real_color)
-    fig = plt.figure(figsize=(15, 8))
-    for i in range(5):
-        ax = plt.subplot(3, 5, i + 1)
-        ax.imshow(L[i][0].cpu(), cmap='gray')
-        ax.axis("off")
-        ax = plt.subplot(3, 5, i + 1 + 5)
-        ax.imshow(fake_imgs[i])
-        ax.axis("off")
-        ax = plt.subplot(3, 5, i + 1 + 10)
-        ax.imshow(real_imgs[i])
-        ax.axis("off")
-    plt.show()
-    if save:
-        fig.savefig(f"colorization_{time.time()}.png")
 
 def log_results(loss_meter_dict):
     for loss_name, loss_meter in loss_meter_dict.items():
